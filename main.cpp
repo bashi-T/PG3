85b83702567aee6be16ef5e952e83b401e198925
MasamiTuzukibashi <k022g0080@g.neec.ac.jp>
2023-10-23 12:05:20 +0900

    01_02確認課題

--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
 #include<stdio.h>
 
-int Recursive(int n)
+int CalcRecursive(int n)
 {
 	if (n <= 1)
 	{
 		return 1;
 	}
-	return(n * Recursive(n - 1));
+	return(n * CalcRecursive(n - 1));
 }
 
-int RecursiveSalary(int n)
+int CalcRecursiveSalary(int n)
 {
 	if (n <= 1)
 	{
 		return 100;
 	}
-	return(2 * RecursiveSalary(n - 1) - 50);
+	return(2 * CalcRecursiveSalary(n - 1) - 50);
 }
 
 int main()
 {
 	int worktime = 10;
-	printf("一般的な賃金:%d\n", 1072 * worktime);
-	//RecursiveSalary(worktime);
-	printf("再帰的な賃金\n");
+	int normalSalary = 1072;
+	printf("一般的な賃金\n");
 	for (int i = 1; i <= worktime; i++)
 	{
-		printf("%d時間目:%d\n",i, RecursiveSalary(i));
+		printf("%d時間分:%d\n", i, normalSalary * i);
 	}
-
+	printf("\n再帰的な賃金\n");
+	for (int i = 1; i <= worktime; i++)
+	{
+		printf("%d時間目:%d\n", i, CalcRecursiveSalary(i));
+	}
 
 	return 0;
 }
