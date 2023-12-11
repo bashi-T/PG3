b061df83edb0f2e52caee2bb6a6d52b377db3863
MasamiTuzukibashi <k022g0080@g.neec.ac.jp>
2023-12-11 12:51:31 +0900

    05_02確認課題（仮）

--- main.cpp
+++ main.cpp
@@ -5,55 +5,276 @@
 #include<functional>
 #include<list>
 #include<iostream>
+#include<algorithm>
+#include<vector>
 
 int main()
 {
-	std::list<const char*>lst
+	std::list<const char*>studentAddress
 	{
-		"Tokyo", "Yurakucho", "Shimbashi", "Hamamatucho",
-		"Tamachi", "Shinagawa", "Osaki", "Gotanda", "Meguro",
-		"Ebisu", "Shibuya", "Harajuku", "Yoyogi", "Shinjuku",
-		"Shin-Okubo", "takadanobaba", "mejiro", "Ikebukuro",
-		"Otuka", "Sugamo", "Komagome", "Tabata", "Nippori",
-		"Uguisudani", "Ueno", "Okachimachi", "Akibahara", "Kanda"
+		"k022g0108@g.neec.ac.jp",
+		"k022g0045@g.neec.ac.jp",
+		"k022g0007@g.neec.ac.jp",
+		"k022g0015@g.neec.ac.jp",
+		"k022g0028@g.neec.ac.jp",
+		"k022g0049@g.neec.ac.jp",
+		"k022g0003@g.neec.ac.jp",
+		"k022g0044@g.neec.ac.jp",
+		"k022g0072@g.neec.ac.jp",
+		"k022g0055@g.neec.ac.jp",
+		"k021g1139@g.neec.ac.jp",
+		"k021g1485@g.neec.ac.jp",
+		"k021g1162@g.neec.ac.jp",
+		"k021g1278@g.neec.ac.jp",
+		"k020g1229@g.neec.ac.jp",
+		"k020g1202@g.neec.ac.jp",
+		"k020g1215@g.neec.ac.jp",
+		"k020g1416@g.neec.ac.jp",
+		"k020g1245@g.neec.ac.jp",
+		"k020g1506@g.neec.ac.jp",
+		"k020g1401@g.neec.ac.jp",
+		"k022g0089@g.neec.ac.jp",
+		"k022g0107@g.neec.ac.jp",
+		"k022g0135@g.neec.ac.jp",
+		"k022g0020@g.neec.ac.jp",
+		"k022g0046@g.neec.ac.jp",
+		"k022g0126@g.neec.ac.jp",
+		"k022g0068@g.neec.ac.jp",
+		"k022g1060@g.neec.ac.jp",
+		"k022g0019@g.neec.ac.jp",
+		"k022g0013@g.neec.ac.jp",
+		"k022g0024@g.neec.ac.jp",
+		"k022g0134@g.neec.ac.jp",
+		"k022g0123@g.neec.ac.jp",
+		"k022g0030@g.neec.ac.jp",
+		"k022g0053@g.neec.ac.jp",
+		"k022g0023@g.neec.ac.jp",
+		"k022g0041@g.neec.ac.jp",
+		"k022g0063@g.neec.ac.jp",
+		"k022g0059@g.neec.ac.jp",
+		"k022g0078@g.neec.ac.jp",
+		"k022g0011@g.neec.ac.jp",
+		"k022g0040@g.neec.ac.jp",
+		"k022g0088@g.neec.ac.jp",
+		"k022g0099@g.neec.ac.jp",
+		"k022g0060@g.neec.ac.jp",
+		"k022g0032@g.neec.ac.jp",
+		"k022g0087@g.neec.ac.jp",
+		"k022g0016@g.neec.ac.jp",
+		"k022g0116@g.neec.ac.jp",
+		"k022g0056@g.neec.ac.jp",
+		"k022g0009@g.neec.ac.jp",
+		"k022g0034@g.neec.ac.jp",
+		"k022g0062@g.neec.ac.jp",
+		"k022g0102@g.neec.ac.jp",
+		"k022g0113@g.neec.ac.jp",
+		"k022g0064@g.neec.ac.jp",
+		"k022g0080@g.neec.ac.jp",
+		"k022g0114@g.neec.ac.jp",
+		"k022g0025@g.neec.ac.jp",
+		"k022g0095@g.neec.ac.jp",
+		"k022g0121@g.neec.ac.jp",
+		"k022g0021@g.neec.ac.jp",
+		"k022g0104@g.neec.ac.jp",
+		"k022g0101@g.neec.ac.jp",
+		"k022g0130@g.neec.ac.jp",
+		"k022g0036@g.neec.ac.jp",
+		"k022g0004@g.neec.ac.jp",
+		"k022g0093@g.neec.ac.jp",
+		"k022g0038@g.neec.ac.jp",
+		"k022g0069@g.neec.ac.jp",
+		"k022g0029@g.neec.ac.jp",
+		"k022g0010@g.neec.ac.jp",
+		"k022g0083@g.neec.ac.jp",
+		"k022g0086@g.neec.ac.jp",
+		"k022g0129@g.neec.ac.jp",
+		"k022g0048@g.neec.ac.jp",
+		"k022g0027@g.neec.ac.jp",
+		"k022g0103@g.neec.ac.jp",
+		"k022g0117@g.neec.ac.jp",
+		"k022g0085@g.neec.ac.jp",
+		"k022g0052@g.neec.ac.jp",
+		"k022g0073@g.neec.ac.jp",
+		"k022g0065@g.neec.ac.jp",
+		"k022g0091@g.neec.ac.jp",
+		"k022g0136@g.neec.ac.jp",
+		"k022g0079@g.neec.ac.jp",
+		"k022g0017@g.neec.ac.jp",
+		"k022g0097@g.neec.ac.jp",
+		"k022g0077@g.neec.ac.jp",
+		"k022g0050@g.neec.ac.jp",
+		"k022g0033@g.neec.ac.jp",
+		"k022g0039@g.neec.ac.jp",
+		"k022g0118@g.neec.ac.jp",
+		"k022g0112@g.neec.ac.jp",
+		"k022g0120@g.neec.ac.jp",
+		"k022g0127@g.neec.ac.jp",
+		"k021g1464@g.neec.ac.jp",
+		"k022g0137@g.neec.ac.jp",
+		"k022g0066@g.neec.ac.jp",
+		"k022g0051@g.neec.ac.jp",
+		"k022g0111@g.neec.ac.jp",
+		"k022g0094@g.neec.ac.jp",
+		"k022g0128@g.neec.ac.jp",
+		"k022g0109@g.neec.ac.jp",
+		"k022g0124@g.neec.ac.jp",
+		"k022g0084@g.neec.ac.jp",
+		"k022g0082@g.neec.ac.jp",
+		"k022g0122@g.neec.ac.jp",
+		"k022g0061@g.neec.ac.jp",
+		"k022g0031@g.neec.ac.jp",
+		"k022g0076@g.neec.ac.jp",
+		"k022g0002@g.neec.ac.jp",
+		"k022g0090@g.neec.ac.jp",
+		"k022g0074@g.neec.ac.jp",
+		"k022g0012@g.neec.ac.jp",
+		"k022g0001@g.neec.ac.jp"
 	};
 
-	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
-		std::cout << *itr << "->";
-		if (*itr == "Kanda")
+	std::vector<int> studentNumber =
+	{
+		108,
+		45,
+		7,
+		15,
+		28,
+		49,
+		3,
+		44,
+		72,
+		55,
+		1139,
+		1485,
+		1162,
+		1278,
+		1229,
+		1202,
+		1215,
+		1416,
+		1245,
+		1506,
+		1401,
+		89,
+		107,
+		135,
+		20,
+		46,
+		126,
+		68,
+		1060,
+		19,
+		13,
+		24,
+		134,
+		123,
+		30,
+		53,
+		23,
+		41,
+		63,
+		59,
+		78,
+		11,
+		40,
+		88,
+		99,
+		60,
+		32,
+		87,
+		16,
+		116,
+		56,
+		9,
+		34,
+		62,
+		102,
+		113,
+		64,
+		80,
+		114,
+		25,
+		95,
+		121,
+		21,
+		104,
+		101,
+		130,
+		36,
+		4,
+		93,
+		38,
+		69,
+		29,
+		10,
+		83,
+		86,
+		129,
+		48,
+		27,
+		103,
+		117,
+		85,
+		52,
+		73,
+		65,
+		91,
+		136,
+		79,
+		17,
+		97,
+		77,
+		50,
+		33,
+		39,
+		118,
+		112,
+		120,
+		127,
+		1464,
+		137,
+		66,
+		51,
+		111,
+		94,
+		128,
+		109,
+		124,
+		84,
+		82,
+		122,
+		61,
+		31,
+		76,
+		2,
+		90,
+		74,
+		12,
+		1
+	};
+
+	std::sort(studentNumber.begin(), studentNumber.end());
+
+	//for (auto itr = studentAddress.begin(); itr != studentAddress.end(); ++itr) {
+	//	std::cout << *itr << "\n";
+	//}
+
+	for (auto itr = studentNumber.begin(); itr != studentNumber.end(); ++itr) {
+		if(*itr<10)
 		{
-			std::cout << "Tokyo\n\n";
+			std::cout << "k022g000" << *itr << "@g.neec.ac.jp\n";
+		}else if(*itr >= 10&&*itr<100)
+		{
+			std::cout << "k022g00" << *itr << "@g.neec.ac.jp\n";
+		}
+		else if (*itr >= 100 && *itr < 1000)
+		{
+			std::cout << "k022g0" << *itr << "@g.neec.ac.jp\n";
+		}
+		else
+		{
+			std::cout << "k022g" << *itr << "@g.neec.ac.jp\n";
 		}
 	}
-
-	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
-		if (*itr == "Nippori")
-		{
-			itr = lst.insert(itr,"Nishi-Nippori");
-			std::cout << *itr << "->";
-			++itr;
-		}
-		std::cout << *itr << "->";
-		if (*itr == "Kanda")
-		{
-			std::cout << "Tokyo\n\n";
-		}
-	}
-
-	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
-		if (*itr == "Shinagawa")
-		{
-			itr = lst.insert(itr, "Takanawa Gateway");
-			std::cout << *itr << "->";
-			++itr;
-		}
-		std::cout << *itr << "->";
-		if (*itr == "Kanda")
-		{
-			std::cout << "Tokyo\n";
-		}
-	}
-
 
 	return 0;
 }
