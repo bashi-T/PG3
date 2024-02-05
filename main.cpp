41307ff0d7eb3f39824ee28c6ca0819f11159755
MasamiTuzukibashi <k022g0080@g.neec.ac.jp>
2024-02-05 11:55:59 +0900

    ittan

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #pragma once
 #include<stdio.h>
 #include<thread>
+#include<string>
+//#include<chrono>
 
 int main()
 {
-	std::thread th1(printf, "thread1\n");
-	th1.join();
-	std::thread th2(printf, "thread2\n");
-	th2.join();
-	std::thread th3(printf, "thread3\n");
-	th3.join();
+	std::string  a(100000, 'a');
+	std::chrono::system_clock::time_point start, end;
+	start = std::chrono::system_clock::now();
+	std::string b1 = a;
+	end = std::chrono::system_clock::now();
+	double elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+	printf("%lf\n", elapsed);
+	std::string b2 = a;
+	std::move(b2);
 
 	return 0;
 }
